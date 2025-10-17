0d06c54d8c488a6073557c4bca3e993cb4d8d635
markuserik <markus13671@gmail.com>
2025-10-17 09:48:37 +0200

    Add try for opening file and discard gpa deinit result

--- src/png.zig
+++ src/png.zig
@@ -7,7 +7,7 @@ pub const Png = struct {
 };
 
 pub fn parseFileFromPath(file_path: []const u8) !Png {
-    const file: fs.File = fs.cwd().openFile(file_path, .{});
+    const file: fs.File = try fs.cwd().openFile(file_path, .{});
     defer file.close();
 
     return parseFile(file);
@@ -16,7 +16,7 @@ pub fn parseFileFromPath(file_path: []const u8) !Png {
 pub fn parseFile(file: fs.File) !Png {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator: std.mem.Allocator = gpa.allocator();
-    defer gpa.deinit();
+    defer _ = gpa.deinit();
 
     const raw_file: []u8 = try allocator.alloc(u8, (try file.stat()).size);
     _ = try file.read(raw_file);
