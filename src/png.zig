f605f54db90aa1800a3b01fce4559f3b5d42cb06
markuserik <markus13671@gmail.com>
2025-11-02 11:22:04 +0100

    Use u16 for simple pixel, add separate struct with 8 bit values

--- src/png.zig
+++ src/png.zig
@@ -59,72 +59,86 @@ fn parsePixels(raw_png: *Raw) ![][]Pixel {
             switch (raw_png.ihdr.color_type) {
                 .Greyscale => {
                     if (bit_depth == 8) {
-                        pixels[y][x].r = raw_pixels[y][x].greyscale8.?;
-                        pixels[y][x].g = raw_pixels[y][x].greyscale8.?;
-                        pixels[y][x].b = raw_pixels[y][x].greyscale8.?;
+                        pixels[y][x].r = val8to16(raw_pixels[y][x].greyscale8.?);
+                        pixels[y][x].g = val8to16(raw_pixels[y][x].greyscale8.?);
+                        pixels[y][x].b = val8to16(raw_pixels[y][x].greyscale8.?);
                     }
                     else if(bit_depth == 16) {
-                        pixels[y][x].r = val16to8(raw_pixels[y][x].greyscale16.?);
-                        pixels[y][x].g = val16to8(raw_pixels[y][x].greyscale16.?);
-                        pixels[y][x].b = val16to8(raw_pixels[y][x].greyscale16.?);
+                        pixels[y][x].r = raw_pixels[y][x].greyscale16.?;
+                        pixels[y][x].g = raw_pixels[y][x].greyscale16.?;
+                        pixels[y][x].b = raw_pixels[y][x].greyscale16.?;
                     }
                     else return error.BitDepthNotImplemented;
                 },
                 .Truecolor => {
                     if (bit_depth == 8) {
-                        pixels[y][x].r = raw_pixels[y][x].r8.?;
-                        pixels[y][x].g = raw_pixels[y][x].g8.?;
-                        pixels[y][x].b = raw_pixels[y][x].b8.?;
-                        pixels[y][x].a = 255;
+                        pixels[y][x].r = val8to16(raw_pixels[y][x].r8.?);
+                        pixels[y][x].g = val8to16(raw_pixels[y][x].g8.?);
+                        pixels[y][x].b = val8to16(raw_pixels[y][x].b8.?);
+                        pixels[y][x].a = 65535;
                     }
                     else {
-                        pixels[y][x].r = val16to8(raw_pixels[y][x].r16.?);
-                        pixels[y][x].g = val16to8(raw_pixels[y][x].g16.?);
-                        pixels[y][x].b = val16to8(raw_pixels[y][x].b16.?);
-                        pixels[y][x].a = 255;
+                        pixels[y][x].r = raw_pixels[y][x].r16.?;
+                        pixels[y][x].g = raw_pixels[y][x].g16.?;
+                        pixels[y][x].b = raw_pixels[y][x].b16.?;
+                        pixels[y][x].a = 65535;
                     }
                 },
                 .Indexed_color => {
                     if (bit_depth == 8) {
                         const entry = raw_png.plte.?.entries[raw_pixels[y][x].index.?];
-                        pixels[y][x].r = entry.r;
-                        pixels[y][x].g = entry.g;
-                        pixels[y][x].b = entry.b;
+                        pixels[y][x].r = val8to16(entry.r);
+                        pixels[y][x].g = val8to16(entry.g);
+                        pixels[y][x].b = val8to16(entry.b);
+                        pixels[y][x].a = 65535;
                     }
                     else return error.BitDepthNotImplemented;
                 },
                 .Greyscale_with_alpha => {
                     if (bit_depth == 8) {
-                        pixels[y][x].r = raw_pixels[y][x].greyscale8.?;
-                        pixels[y][x].g = raw_pixels[y][x].greyscale8.?;
-                        pixels[y][x].b = raw_pixels[y][x].greyscale8.?;
-                        pixels[y][x].a = raw_pixels[y][x].alpha8.?;
+                        pixels[y][x].r = val8to16(raw_pixels[y][x].greyscale8.?);
+                        pixels[y][x].g = val8to16(raw_pixels[y][x].greyscale8.?);
+                        pixels[y][x].b = val8to16(raw_pixels[y][x].greyscale8.?);
+                        pixels[y][x].a = val8to16(raw_pixels[y][x].alpha8.?);
                     }
                     else if(bit_depth == 16) {
-                        pixels[y][x].r = val16to8(raw_pixels[y][x].greyscale16.?);
-                        pixels[y][x].g = val16to8(raw_pixels[y][x].greyscale16.?);
-                        pixels[y][x].b = val16to8(raw_pixels[y][x].greyscale16.?);
-                        pixels[y][x].a = val16to8(raw_pixels[y][x].alpha16.?);
+                        pixels[y][x].r = raw_pixels[y][x].greyscale16.?;
+                        pixels[y][x].g = raw_pixels[y][x].greyscale16.?;
+                        pixels[y][x].b = raw_pixels[y][x].greyscale16.?;
+                        pixels[y][x].a = raw_pixels[y][x].alpha16.?;
                     }
                 },
                 .Truecolor_with_alpha => {
                     if (bit_depth == 8) {
-                        pixels[y][x].r = raw_pixels[y][x].r8.?;
-                        pixels[y][x].g = raw_pixels[y][x].g8.?;
-                        pixels[y][x].b = raw_pixels[y][x].b8.?;
-                        pixels[y][x].a = raw_pixels[y][x].alpha8.?;
+                        pixels[y][x].r = val8to16(raw_pixels[y][x].r8.?);
+                        pixels[y][x].g = val8to16(raw_pixels[y][x].g8.?);
+                        pixels[y][x].b = val8to16(raw_pixels[y][x].b8.?);
+                        pixels[y][x].a = val8to16(raw_pixels[y][x].alpha8.?);
                     }
                     else {
-                        pixels[y][x].r = val16to8(raw_pixels[y][x].r16.?);
-                        pixels[y][x].g = val16to8(raw_pixels[y][x].g16.?);
-                        pixels[y][x].b = val16to8(raw_pixels[y][x].b16.?);
-                        pixels[y][x].a = val16to8(raw_pixels[y][x].alpha16.?);
+                        pixels[y][x].r = raw_pixels[y][x].r16.?;
+                        pixels[y][x].g = raw_pixels[y][x].g16.?;
+                        pixels[y][x].b = raw_pixels[y][x].b16.?;
+                        pixels[y][x].a = raw_pixels[y][x].alpha16.?;
                     }
                 }
             }
         }
     }
+
+    for (0..height) |y| {
+        for (0..width) |x| {
+            pixels[y][x].pixel8bit.r = val16to8(pixels[y][x].r);
+            pixels[y][x].pixel8bit.g = val16to8(pixels[y][x].g);
+            pixels[y][x].pixel8bit.b = val16to8(pixels[y][x].b);
+            pixels[y][x].pixel8bit.a = val16to8(pixels[y][x].a);
+        }
+    }
     return pixels;
+}
+
+fn val8to16(val: u8) u16 {
+    return @intCast(val * @as(u16, 257));
 }
 
 fn val16to8(val: u16) u8 {
@@ -132,6 +146,14 @@ fn val16to8(val: u16) u8 {
 }
 
 pub const Pixel = struct {
+    r: u16,
+    g: u16,
+    b: u16,
+    a: u16,
+    pixel8bit: Pixel8Bit
+};
+
+pub const Pixel8Bit= struct {
     r: u8,
     g: u8,
     b: u8,
